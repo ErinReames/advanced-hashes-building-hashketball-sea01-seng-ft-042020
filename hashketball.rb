f24207311d503190f08935387b254f66561874ac
Erin Reames <italiagirl83@gmail.com>
2020-04-06 23:48:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -12,7 +12,9 @@ def game_hash
           :points => 22,
           :rebounds => 12,
           :assists => 12,
-          
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1 
         }
         ]
     }
