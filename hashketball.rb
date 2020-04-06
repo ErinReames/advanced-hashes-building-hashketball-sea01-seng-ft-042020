9adbd1de9b25c8c6af978102649db600f4481737
Erin Reames <italiagirl83@gmail.com>
2020-04-06 23:52:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -27,8 +27,8 @@ def game_hash
             :blocks => 12,
             :slam_dunks => 7 
         },
-        :player_name => "Brook Lopez",
-            :numbere => 11,
+        {:player_name => "Brook Lopez",
+            :number => 11,
             :shoe => 17,
             :points => 17,
             :rebounds => 19,
@@ -36,6 +36,21 @@ def game_hash
             :steals => 3,
             :blocks => 1,
             :slam_dunks => 15 
+        },
+        {
+          {:player_name => "Mason Plumlee",
+            :number => 1,
+            :shoe => 19,
+            :points => 26,
+            :rebounds => 11,
+            :assists => 6,
+            :steals => 3,
+            :blocks => 8,
+            :slam_dunks => 5 
+        },
+        {
+          
+        }
         ]
     }
     :away =>{
